38f6f88f3a6377246bbaf5de756697291449afb7
Brett Owers <owersbrett@gmail.com>
2019-12-05 12:56:34 -0700

    made da box

--- answer.js
+++ answer.js
@@ -1,1 +1,23 @@
 // *** YOUR ANSWER BELOW ***
+
+function makeBox(x,y){
+
+    let minusSigns = "";
+    let spaces = "";
+
+    while (minusSigns.length !== x){
+        minusSigns += "-";
+    }
+
+    while (spaces.length !== x){
+        spaces += " ";
+    }
+
+    console.log(`+${minusSigns}+`);
+    for (let i = 0; i !== y; i++){
+        console.log(`|${spaces}|`);
+    }
+    console.log(`+${minusSigns}+`);
+}
+
+makeBox(52,6);
